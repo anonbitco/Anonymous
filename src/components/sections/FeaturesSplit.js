59d81dad355679fca4056fc13a09e4d763cde84f
Krishna Dahal <krishna.dahal2017@gmail.com>
2021-07-06 08:41:13 -0400

    typo fix

--- src/components/sections/FeaturesSplit.js
+++ src/components/sections/FeaturesSplit.js
@@ -83,7 +83,7 @@ const FeaturesSplit = ({
                 )}
                 data-reveal-container=".split-item">
                 <Image
-                  src={require('./../../assets/images/features-split-image-01.png')}
+                  src={require('./../../assets/images/features-split-image-01.jpeg')}
                   alt="Features split 01"
                   width={528}
                   height={396} />
