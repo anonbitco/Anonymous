e75a3bb98dbe2e09bef000a4e950a1db7375f076
Anonbitco <81495279+anonbitco@users.noreply.github.com>
2021-12-27 03:26:04 -0500

    Update Logo.js

--- src/components/layout/partials/Logo.js
+++ src/components/layout/partials/Logo.js
@@ -23,8 +23,8 @@ const Logo = ({
           <Image
             src={require('./../../../assets/images/logo.png')}
             alt="Open"
-            width={70}
-            height={70} />
+            width={100}
+            height={100} />
         </Link>
       </h3>
     </div>
